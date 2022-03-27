060c7d91a6d6d286bc14bb70b06dceab9e05677d
Tom Tichy <tom@steamchain.io>
2022-03-27 10:11:40 -0400

    tweaked recursive version

--- solution6.py
+++ solution6.py
@@ -1,24 +1,95 @@
-def reduce(x,y,count=0):
-    if x * y == 1:
-        return str(count)
-    if y == 0: return 'impossible'
+from time import time
+import timeit
+
+
+def shortcut(x,y):
     bigger = max(x,y)
     smaller = min(x, y)
     if smaller !=1 and bigger != 1:
         div = bigger / smaller
         left = bigger % smaller
     else:
-        div=1
-        left=bigger-smaller
-    count=count+div
+        div=bigger-1
+        left=1
+
+    return (div,left)
+
+
+def reduce(x,y,count=0):
+    if x * y == 1:
+        return str(count)
+    if y == 0: return 'impossible'
+
+    if y==1: return str(count+x-1)
+
+    if abs(x-y)==1:
+        return str(count+y)
+    
+    div = x // y
+    left = x % y
+    count +=div
+
+    if left > y:
+        return reduce(left,y,count)
+    return reduce(y,left,count)
+
+
+    # if x>y:
+    #     div=x // y
+    #     left = x % y
+    #     return reduce(y,left,count+div)
+    # else:
+    #     div = y // x
+    #     left = y % x
+    #     return reduce(x,left,count+div)
+
+    # bigger = max(x,y)
+    # smaller = min(x, y)
+    # if smaller !=1 and bigger != 1:
+    #     div = bigger // smaller
+    #     left = bigger % smaller
+    # else:
+    #     # shortcut cases like 998:1, otherwise stack gets busted
+    #     # div=bigger-1
+    #     # left=1
+    #     return (str(count+bigger-1))
+    count +=div
     return reduce(smaller,left,count)
 
 
 def solution(M,F):
     # type: (str,str)->str
     if M == F: return 'impossible'
-    macs = int(float(M.replace('^','E')))
-    facs = int(float(F.replace('^','E')))
-   
-    return reduce(macs,facs)
+    macs = int(M)
+    facs = int(F)
 
+    larger = max(macs,facs)
+    smaller = min(macs,facs)    
+    return reduce(larger,smaller)
+
+def solution2(M, F):
+    goal = (int(M), int(F))
+    x, y = goal
+    c = 0
+    while x!=y:
+        if x > y:
+            num_subs = (x-y)//y + ((x-y) % y > 0)
+            c += num_subs
+            x, y = x - num_subs * y, y
+        elif y > x:
+            num_subs = (y-x)//x + ((y-x) % x > 0)
+            c += num_subs
+            x, y = x, y - num_subs * x
+        
+    return str(c) if (x, y)==(1, 1) else 'impossible'
+
+
+
+def timing(M,F):
+    t0=timeit.default_timer()
+    print(solution(M,F))
+    print('mine elapsed: {}'.format(timeit.default_timer()-t0))
+
+    t0=timeit.default_timer()
+    print(solution2(M,F))
+    print('his elapsed: {}'.format(timeit.default_timer()-t0))
--- test_solution6.py
+++ test_solution6.py
@@ -1,6 +1,6 @@
 import unittest
 import solution6
-from fractions import Fraction
+import time
 
 class TestSolution6(unittest.TestCase):
 
@@ -53,24 +53,58 @@ class TestSolution6(unittest.TestCase):
 
     def test_solution_with_17_20_should_be_8(self):
         result = solution6.solution('17','20')
-
+        result2 = solution6.solution2('17','20')
+        self.assertEqual(result2,result)
         self.assertEqual(result,'8')             
 
 
     def test_solution_with_18_21(self):
         result = solution6.solution('18','21')
         self.assertEqual(result,'impossible')
+        result2 = solution6.solution2('18','21')
+        self.assertEqual(result2,result)
 
     def test_solution_with_11_4_should_be_5(self):
         result = solution6.solution('11','4')
         self.assertEqual(result,'5')
+        result2 = solution6.solution2('11','4')
+        self.assertEqual(result2,result)
 
+
+    def test_solution_with_100_1_should_be_99(self):
+        result = solution6.solution('100','1')
+        result2 = solution6.solution2('100','1')
+        self.assertEqual(result2,result)
+        self.assertEqual(result,'99')
 
     def test_solution_with_large_numbers(self):
         result = solution6.solution('21205',str(21205+9031))
         self.assertEqual(result,'29')
+        result2 = solution6.solution2('21205',str(21205+9031))
+        self.assertEqual(result2,result)
 
+
+    def test_solution_with_large_numbers3(self):
+        result2=solution6.solution2('10000000','9999')
+        result = solution6.solution('10000000','9999')
+        self.assertEqual(result,result2)
+
+    def test_solution_with_1(self):
+        result2 = solution6.solution2('10','1')
 
     def test_solution_with_large_numbers2(self):
+        t0_mine=time.time()
         result = solution6.solution('10^15','12459852189')
+        t1_mine = time.time()
+        print('mine: {}'.format(t1_mine-t0_mine))
+
+        t0_his = time.time()
+        result2 = solution6.solution2('10000000000000000','12459852189')
+        t1_his = time.time()
+        print('his: {}'.format(t1_his-t0_his))
+
+        self.assertEqual(result2,result)
         self.assertEqual(result,'802761') # not sure about the result here          
+
+    def test_timing(self):
+        solution6.timing('1000000','9999')
